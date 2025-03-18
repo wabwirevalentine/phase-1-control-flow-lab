6bc28d8443297f318a6ad7411e21b7e0b3d9d3f2
wabwirevalentine <valentine.wabwire@tracom.co.ke>
2025-03-18 09:30:29 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 2500) {
+    return "No can do.";
+  } else if (someValue > 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (400 < someValue < 2000) {
+    return "That will be twenty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someValue) {
   // Write your code here!
+  if (someValue === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someValue) {
   // Write your code here!
+  if (someValue === "generous") {
+    return "Thank you so much.";
+  } else if (someValue === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
